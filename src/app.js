843980c73a50bf4d566c98edd732b10bb80aa0e2
thalesleite <thalesaleite@gmail.com>
2020-08-13 12:22:09 +0100

    added bodyparser

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const { erros } = require('celebrate');
 const routes = require('./routes');
@@ -9,12 +10,13 @@ const app = express();
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
-app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
