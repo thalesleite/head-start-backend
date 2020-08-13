0ed57583bb362c0442b69593be3f56aa0b6021d9
thalesleite <thalesaleite@gmail.com>
2020-08-13 12:00:06 +0100

    enabled cors origin

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ const { erros } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
-
-app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+app.use(cors({
+  origin: '*',
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(routes);
 
