f3decdde85b128c778001d2baac9f9dedb234536
thalesleite <thalesaleite@gmail.com>
2020-08-13 11:42:42 +0100

    added function for enable cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(routes);
 
 module.exports = app;
