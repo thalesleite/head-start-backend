ed119a12eab92a14f5b57729daa28fd6541b6476
thalesleite <thalesaleite@gmail.com>
2020-08-15 09:54:04 +0100

    added cors(corsOptions)

--- src/app.js
+++ src/app.js
@@ -5,18 +5,25 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+// app.use((req, res, next) => {
+//   // res.header('Access-Control-Allow-Origin', '*');
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  next();
-});
+//   next();
+// });
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(routes);
 
 module.exports = app;
