cf145a02cd69d0800199ac771750002a1785f090
thalesleite <thalesaleite@gmail.com>
2020-08-15 09:12:40 +0100

    added header = * to allow origin

--- src/app.js
+++ src/app.js
@@ -14,9 +14,14 @@ const app = express();
 //   origin: '*',
 //   optionsSuccessStatus: 200
 // }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(routes);
 
 module.exports = app;
