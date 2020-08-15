e1a5e5186b4a406c5d715acb09a55a5ae9aeac5f
thalesleite <thalesaleite@gmail.com>
2020-08-15 10:48:22 +0100

    added app.use(cors())

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { erros } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 module.exports = app;
