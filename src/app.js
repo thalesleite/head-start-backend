95e0bb9e06afd771b3306252d1a893017cfb16de
thalesleite <thalesaleite@gmail.com>
2020-08-15 10:35:04 +0100

    added res.header(Access-Control-Allow-Origin, *);

--- src/app.js
+++ src/app.js
@@ -5,22 +5,11 @@ const routes = require('./routes');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   // res.header('Access-Control-Allow-Origin', '*');
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   next();
-// });
-
-const corsOptions = {
-  origin: 'https://git.heroku.com/head-start-app.git',
-  optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 //app.use(cors());
