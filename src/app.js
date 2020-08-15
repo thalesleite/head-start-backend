c1282cce468e3824851292bbfc45be393c53b7d2
thalesleite <thalesaleite@gmail.com>
2020-08-15 09:40:22 +0100

    added res.setHeader('Access-Control-Allow-Origin', '*')

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', '*');
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
   next();
 });
 
