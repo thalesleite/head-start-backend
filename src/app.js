22e3bd7ce349208c01d8cc1daf2545d959273550
thalesleite <thalesaleite@gmail.com>
2020-08-15 09:27:31 +0100

    added res.setHeader('Access-Control-Allow-Origin', '*')

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const { erros } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-// app.use(cors({
-//   origin: '*',
-//   optionsSuccessStatus: 200
-// }));
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
